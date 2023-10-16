3c0b9294081c6ac18f3c12b4399de26ed7cd164f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-16 07:11:05 +0000

    hero section done

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
@@ -13,12 +13,12 @@ const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border cursor-pointer ${variant}`}
       title={title}
       id={id}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap" htmlFor={id}>
+      <label className="bold-16 whitespace-nowrap cursor-pointer" htmlFor={id}>
         {title}
       </label>
     </button>
